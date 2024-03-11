0da6dcea809b10408c280c8b4413a9e4025d8e39
Firstp1ck <kortharshadowbreath@gmail.com>
2024-03-11 22:24:51 +0100

    Update Youtube_down_con_01.bat

--- Youtube_down_con_01.bat
+++ Youtube_down_con_01.bat
@@ -1,3 +1,3 @@
 @echo off
-python "D:\Benutzer\Geschäft\OneDrive - KUALA LUMPUR KEPONG BERHAD\Privat\Python\Projekte\Youtube_down_con\Youtube_down_con_01.py"
+python "D:\Benutzer\Firstpick\Python\Projekte\Youtube_down_con\Youtube_down_con_01.py"
 pause
