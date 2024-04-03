6f5198a221160dca0119b7ff4adb18137d8bd9e6
Firstp1ck <kortharshadowbreath@gmail.com>
2024-04-03 23:11:06 +0200

    Update START_Youtube_down_con_01.bat

--- START_Youtube_down_con_01.bat
+++ START_Youtube_down_con_01.bat
@@ -1,3 +1,19 @@
 @echo off
-python "D:\Benutzer\Firstpick\Python\Projekte\Youtube_down_con\Youtube_down_con_01.py"
+:: Überprüfen, ob die Pakete bereits installiert ist
+python -m pip list | findstr /C:"pytube" > nul
+python -m pip list | findstr /C:"moviepy" > nul
+
+:: Wenn das Paket nicht gefunden wurde, führe die Installation aus
+if errorlevel 1 (
+    echo Pakete werden installiert...
+    python -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pytube moviepy
+) else (
+    echo Pakete sind bereits installiert.
+)
+
+:: Führen Sie Ihr Python-Skript aus. Ersetzen Sie 'APP.py' durch den Namen Ihres Skripts.
+python Youtube_down_con_01.py
+
+echo.
+echo Skript wurde ausgeführt. Fenster schließen oder weitere Befehle eingeben.
 pause
